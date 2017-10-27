64aa83d7927b2517fc19038e925f253da7681b4c
vinaybayyaram <vinaybayyaram@gmail.com>
2017-10-27 15:43:19 +0530

    Update SampleOne.java
    updated by user2

--- newjavaproject/src/newjavaproject/SampleOne.java
+++ newjavaproject/src/newjavaproject/SampleOne.java
@@ -3,8 +3,7 @@ package newjavaproject;
 public class SampleOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hi vinay updated by user");
 	}
 
 }
